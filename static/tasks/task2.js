9d19264d2aaa61958350873f4108a4c0c57e9a8c
Eriktysklind <h23eribl@du.se>
2025-04-11 10:00:46 +0200

    Löst funktionaliteten i task2 igenom att tabort polylineMeasure och skapa upp en instans på nytt vid varje knapp tryck

--- static/tasks/task2.js
+++ static/tasks/task2.js
@@ -1,38 +1,67 @@
+const school1Coords = [
+  { lat: 60.61668314092263, lng: 15.627644549953436 },
+  { lat: 60.625256903746184, lng: 15.622433198014082 },
+  { lat: 60.592595759762666, lng: 15.685269813355674 },
+  { lat: 60.604365663833036, lng: 15.624258467326692 },
+  { lat: 60.60984456541524, lng: 15.638397283032829 },
+];
+const blackIcon = L.icon({
+  iconUrl: "/static/img/map-marker.svg",
+  iconSize: [32, 37], // size of the icon
+  iconAnchor: [32, 37], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
+});
 
 function loadTask2() {
+  mymap.removeControl(polylineMeasure);
+
+  polylineMeasure = L.control
+    .polylineMeasure({
+      position: "topleft",
+      unit: "kilometres",
+      showBearings: true,
+      clearMeasurementsOnStop: false,
+      showClearControl: true,
+      showUnitControl: true,
+    })
+    .addTo(mymap);
+
   mymap.setView([60.606715170380895, 15.635754054322716], 13);
-  const school1Coords = [
-    { lat: 60.61668314092263, lng: 15.627644549953436 },
-    { lat: 60.625256903746184, lng: 15.622433198014082 },
-    { lat: 60.592595759762666, lng: 15.685269813355674 },
-    { lat: 60.604365663833036, lng: 15.624258467326692 },
-    { lat: 60.60984456541524, lng: 15.638397283032829 },
-  ];
-  const blackIcon = L.icon({
-    iconUrl: "/static/img/map-marker.svg",
-    iconSize: [32, 37], // size of the icon
-    iconAnchor: [32, 37], // point of the icon which will correspond to marker's location
-    popupAnchor: [0, -30], // point from which the popup should open relative to the iconAnchor
+  sidebar.show();
+
+  L.marker([60.61668314092263, 15.627644549953436], { icon: blackIcon })
+    .addTo(mymap)
+    .bindPopup("Söderbaumska");
+  L.marker([60.625256903746184, 15.622433198014082], { icon: blackIcon })
+    .addTo(mymap)
+    .bindPopup("Britsarvsskolan");
+  L.marker([60.592595759762666, 15.685269813355674], { icon: blackIcon })
+    .addTo(mymap)
+    .bindPopup("hälsinggårdsskolan");
+  L.marker([60.604365663833036, 15.624258467326692], { icon: blackIcon })
+    .addTo(mymap)
+    .bindPopup("Västraskolan");
+  L.marker([60.60984456541524, 15.638397283032829], { icon: blackIcon })
+    .addTo(mymap)
+    .bindPopup("Mosaik");
+
+  polylineMeasure.seed([school1Coords]);
+
+  $("#btns1").click(function () {
+    mymap.setView([60.61668314092263, 15.627644549953436], 17);
   });
-    sidebar.show();
-    s1 = L.marker([60.61668314092263, 15.627644549953436], { icon: blackIcon })
-      .addTo(mymap)
-      .bindPopup("Söderbaumska");
-    s2 = L.marker([60.625256903746184, 15.622433198014082], { icon: blackIcon })
-      .addTo(mymap)
-      .bindPopup("Britsarvsskolan");
-    s3 = L.marker([60.592595759762666, 15.685269813355674], { icon: blackIcon })
-      .addTo(mymap)
-      .bindPopup("hälsinggårdsskolan");
-    s4 = L.marker([60.604365663833036, 15.624258467326692], { icon: blackIcon })
-      .addTo(mymap)
-      .bindPopup("Västraskolan");
-    s5 = L.marker([60.60984456541524, 15.638397283032829], { icon: blackIcon })
-      .addTo(mymap)
-      .bindPopup("Mosaik");
-
-    polylineMeasure.seed([school1Coords]);
-
+  $("#btns2").click(function () {
+    mymap.setView([60.625256903746184, 15.622433198014082], 17);
+  });
+  $("#btns3").click(function () {
+    mymap.setView([60.592595759762666, 15.685269813355674], 17);
+  });
+  $("#btns4").click(function () {
+    mymap.setView([60.604365663833036, 15.624258467326692], 17);
+  });
+  $("#btns5").click(function () {
+    mymap.setView([60.60984456541524, 15.638397283032829], 17);
+  });
 }
 
 function clearMap() {
@@ -40,7 +69,6 @@ function clearMap() {
     if (layer !== lyrOSM) {
       mymap.removeLayer(layer);
     }
-    
   });
 }
 
@@ -51,7 +79,7 @@ $(document).ready(function () {
   });
 });
 
-$("#btns1").click(function () {
+/* $("#btns1").click(function () {
   mymap.setView([60.61668314092263, 15.627644549953436], 17);
 });
 $("#btns2").click(function () {
@@ -66,3 +94,4 @@ $("#btns4").click(function () {
 $("#btns5").click(function () {
   mymap.setView([60.60984456541524, 15.638397283032829], 17);
 });
+ */
