5b4aa316f3f226c04848677429bb9d0639200303
Eriktysklind <h23eribl@du.se>
2025-05-02 13:45:11 +0200

    kommentare

--- main.py
+++ main.py
@@ -9,16 +9,19 @@ def index():
     return render_template('index.html')
 
 @app.route("/api/clusters")
+#Sätter upp en ny route för att hantera clusters.
 def get_clusters():
+    #Bygger en funktionen get_clusters och börjar med att läsa in csv filen
     df = pd.read_csv("static/data/school_locations.csv")
-
-
+    #Döper om kolumnerna till lat och lon för enkelheten.
     df = df.rename(columns={"ycoord": "lat", "xcoord": "lon"})
-
+    #Ser till att spara ner kordinaterna i variabeln coords och hämtar informantion från datamängden
     coords = df[["lat", "lon"]]
+    #Använder kmeans för klustra skolorna och delar upp de i 3 grupper. 
     kmeans = KMeans(n_clusters=3, random_state=42).fit(coords)
+    #Lägger till en kolumnen i dataframen som heter cluster för att kunna se deras klustertillhörighet. 
     df["cluster"] = kmeans.labels_
-
+    #Retunerar hela dataframen som en JSON-array och varje skola en klustertillhörighet. 
     return jsonify(df.to_dict(orient="records"))
 
 if __name__ == '__main__':
--- static/tasks/task7.js
+++ static/tasks/task7.js
@@ -1,15 +1,21 @@
 function loadTask7() {
     clearMap();
+    //Rensar mappen från tidigare lager och sätter en vy för när task 7 laddas in
     mymap.setView([59.33559401837791, 18.021282737696875], 10);
   
     const colors = ["red", "blue", "green", "orange", "purple"];
-  
+    //Deklarerar en variabel för ett antal färger som används för att visualisera de olika klusterna senare. 
     fetch("/api/clusters")
+    //Hämtar kluster datan från flask där python koden skrivits. 
       .then((res) => res.json())
+      //Omvandlar svaret till JSON
       .then((data) => {
         data.forEach((school) => {
+          //Med hjälp av foreach loopar vi igenom hela datamängden
           const color = colors[school.cluster];
+          //Vi sätter sedan korrekt färg på skolan baserat på vilket kluster den tillhör.
           L.circleMarker([school.lat, school.lon], {
+            //Vi skapar en cirkelmarkör för varje skola och placerar den på kordinaterna som finns i datamängden. 
             radius: 8,
             color: "#000",
             weight: 1,
